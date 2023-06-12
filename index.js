30dac551f69aeba13c8a7c62d256bf04a128b64a
grigaskate <grigaskate@gmail.com>
2023-06-12 18:01:04 +0300

    add error message

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const articlesList = document.querySelector(".articles"); //находим разметку, куда нужно поместить посты
 
 fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => response.json())
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
   .then((json) => {
     json.forEach((elem) => {
       const div = document.createElement("div");
@@ -24,6 +29,7 @@ const formInput = document.querySelector(".form__addArticle");
 const articleTitle = document.querySelector(".article__title");
 const articleText = document.querySelector(".article__text");
 const addArticleBtn = document.querySelector(".article__btn");
+const error = document.querySelector(".error");
 
 //Функция для кнопки "Создать пост"
 addArticleBtn.onclick = function (event) {
@@ -34,6 +40,11 @@ addArticleBtn.onclick = function (event) {
   };
   if (articleTitle.value === "" || articleText.value === "") {
     addArticleBtn.style.display = "block";
+    error.innerHTML = `Заполните поля "Заголовок поста" и "Текст поста"`;
+  }
+  if (articleTitle.value.trim() === "" || articleText.value.trim() === "") {
+    addArticleBtn.style.display = "block";
+    error.innerHTML = `Заполните поля "Заголовок поста" и "Текст поста"`;
   } else {
     fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "POST",
@@ -45,7 +56,6 @@ addArticleBtn.onclick = function (event) {
       .then((response) => response.json())
       .then((article) => {
         const newArticle = document.querySelector(".newArticle");
-        console.log(article);
         const display = `<div class="containerArticle">
                         <p  class="title">Заголовок: ${article.title}</p>
                         <p>${article.body}</p>
@@ -53,6 +63,7 @@ addArticleBtn.onclick = function (event) {
         newArticle.innerHTML += display;
         articleTitle.value = "";
         articleText.value = "";
+        error.innerHTML = ` `;
       })
       .catch((err) => {
         articlesList.innerHTML = `Произoшла ошибка. Обратитесь в техническую поддержку.`;
